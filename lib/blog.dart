d4b02cb2a017b4103509ecc62317df6da1d35519
surapong <surapong@mfu.ac.th>
2021-03-17 21:41:54 +0700

    blog: dummy data

--- lib/blog.dart
+++ lib/blog.dart
@@ -7,6 +7,14 @@ class Blog extends StatefulWidget {
 }
 
 class _BlogState extends State<Blog> {
+  var _data = {
+    'post': [
+      {'blogID': 1, 'title': 'Dummy', 'detail': 'Dummy', 'year': 2021},
+      {'blogID': 2, 'title': 'Dummy2', 'detail': 'Dummy2', 'year': 2021},
+      {'blogID': 3, 'title': 'Dummy3', 'detail': 'Dummy3', 'year': 2021},
+    ]
+  };
+
   //------------- Logout -----------------
   void logout() async {
     // clear the saved token
@@ -24,7 +32,21 @@ class _BlogState extends State<Blog> {
         title: Text('Blog'),
         actions: [IconButton(icon: Icon(Icons.logout), onPressed: logout)],
       ),
-      body: Container(),
+      body: ListView.builder(
+        itemCount: _data['post'].length ?? 0,
+        itemBuilder: (BuildContext context, int index) {
+          return ListTile(
+            leading: Icon(Icons.edit),
+            title: Text(_data['post'][index]['title']),
+            subtitle: Text(_data['post'][index]['detail']),
+            trailing: Icon(Icons.delete),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {},
+      ),
     );
   }
 }
